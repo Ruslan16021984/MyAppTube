5a681b119efb69773a4cb8aebb069cbd1af6618f
Ruslan16021984 <carbit3333333@gmail.com>
2018-08-12 20:00:51 +0300

    Add files via upload

---
+++ java/ruslanann/myappyoutube/activitys/fragments/FragmentVideo.java
@@ -0,0 +1,60 @@
+package ruslanann.myappyoutube.activitys.fragments;
+
+import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+
+import ruslanann.myappyoutube.R;
+
+/**
+ * Created by user on 25.05.2016.
+ */
+public class FragmentVideo extends YouTubePlayerFragment implements YouTubePlayer.OnInitializedListener{
+    private YouTubePlayer player;
+    private String videoId;
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        initialize(getResources().getString(R.string.youtube_apikey), this);
+    }
+    @Override
+    public void onDestroy() {
+        if (player != null) {
+            player.release();
+        }
+        super.onDestroy();
+    }
+
+    public void setVideoId(String videoId) {
+        if (videoId != null && !videoId.equals(this.videoId)) {
+            this.videoId = videoId;
+            if(player != null) {
+                player.cueVideo(videoId);
+            }
+        }
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean b) {
+        this.player = player;
+        player.addFullscreenControlFlag(YouTubePlayer.FULLSCREEN_FLAG_ALWAYS_FULLSCREEN_IN_LANDSCAPE);
+        player.setOnFullscreenListener((YouTubePlayer.OnFullscreenListener) getActivity());
+
+        if (!b && videoId != null) {
+            player.cueVideo(videoId);
+        }
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult result) {
+        this.player = null;
+
+    }
+    public void backnormal() {
+        player.setFullscreen(false);
+    }
+}
---
+++ java/ruslanann/myappyoutube/activitys/utils/MyFirebaseMessagingService.java
@@ -0,0 +1,47 @@
+package ruslanann.myappyoutube.activitys.utils;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import ruslanann.myappyoutube.R;
+import ruslanann.myappyoutube.activitys.activitys.ActivityHome;
+
+/**
+ * Created by user on 03.11.2016.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        sendNotification(remoteMessage.getNotification().getBody());
+
+    }
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, ActivityHome.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_launcher))
+                .setContentTitle(this.getString(R.string.app_name))
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
